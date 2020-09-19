a54d60a10363f87ed86de839ef1187309f03c09e
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-09-19 17:08:16 +0200

    refactor: create own hook HttpRequests

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,41 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 import HelloWorld from '../Components/HelloWorld'
 import Loader from '../Components/Loader'
-import ProductCard from '../Components/ProductCard';
+import ProductCard from '../Components/ProductCard'
+import { useAxiosGet } from '../Hooks/HttpRequests'
 
 function Home() {
     const url = `https://5e8dd95322d8cd0016a79b97.mockapi.io/api/v1/products?page=1&limit=10`
-    const [products, setProducts] = useState({
-        loading: false,
-        data: null,
-        error: false
-    })
-
-    useEffect(() => {
-        setProducts({
-            loading: true,
-            data: null,
-            error: false
-        })
-        axios.get(url)
-            .then(response => {
-                setProducts({
-                    loading: false,
-                    data: response.data,
-                    error: false
-                })
-            })
-            .catch(error => {
-                setProducts({
-                    loading: false,
-                    data: null,
-                    error: true
-                })
-            })
-    }, [url])
-
     
+    let products = useAxiosGet(url)
+
     let content = null
 
 
